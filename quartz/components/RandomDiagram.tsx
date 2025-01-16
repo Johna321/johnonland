0275aea08960b39a14a8a33a63e48162873a923b
John Aitken <johna321123@gmail.com>
2025-01-16 04:22:52 -0500

    Quartz sync: Jan 16, 2025, 4:22 AM

--- quartz/components/RandomDiagram.tsx
+++ quartz/components/RandomDiagram.tsx
@@ -1,17 +1,18 @@
 // @ts-ignore: this is safe
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types";
 import randomdiagramsScript from "./scripts/randomdiagrams.inline";
+import physicsEasterEgg from "./scripts/physicseasteregg.inline";
 import { classNames } from "../util/lang";
 
 const RandomDiagram: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
     return (
-        <div class={classNames(displayClass)} style="margin-left:auto">
+        <div id="diagram-container" class={classNames(displayClass)} style="margin-left:auto;">
             <img 
                 id="random_diagram" 
                 src='https://johnon.land/randomdiagrams/cube_light.svg'
                 alt="cube"
-                height="75px"
-                width="75px"
+                height="60px"
+                width="60px"
                 style="margin-top: 0; margin-bottom: 0"
             /> 
         </div>
@@ -19,5 +20,6 @@ const RandomDiagram: QuartzComponent = ({ displayClass }: QuartzComponentProps) 
 }
 
 RandomDiagram.beforeDOMLoaded = randomdiagramsScript
+RandomDiagram.afterDOMLoaded = physicsEasterEgg
 
 export default (() => RandomDiagram) satisfies QuartzComponentConstructor
